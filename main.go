58e95a39b9765f0b9103299b5b391ecdb9e968fa
tagasouichiro <stitow14@gmail.com>
2017-02-11 16:48:16 +0900

    make init towers

--- main.go
+++ main.go
@@ -19,45 +19,35 @@ var towers = []*list.List{
 
 func main() {
 	fmt.Println("left -> right pool")
-	initTower(towers[left], 3)
+	initTower(towers, 3)
 	printTower(towers[left], towers[center], towers[right])
-	//// 2
-	//// 3 _ 1
-	popPush(towers[left], towers[right])
-	////
-	//// 3 2 1
-	popPush(towers[left], towers[center])
-	////   1
-	//// 3 2 _
-	popPush(towers[right], towers[center])
-	////   1
-	//// _ 2 3
-	popPush(towers[left], towers[right])
-	////
-	//// 1 2 3
-	popPush(towers[center], towers[left])
-	////     2
-	//// 1 _ 3
-	popPush(towers[center], towers[right])
-	////     1
-	////     2
-	//// _ _ 3
-	popPush(towers[left], towers[right])
 
+	threeTower()
+	initTower(towers, 3)
 	printTower(towers[left], towers[center], towers[right])
 }
 
-func initTower(left *list.List, n int) {
+func initTower(towers []*list.List, n int) {
 	for n > 0 {
-		left.PushBack(n)
+		towers[left].PushBack(n)
 		n--
+	}
+
+	for towers[center].Len() != 0 {
+		towers[center].Remove(towers[center].Back())
+	}
+	for towers[right].Len() != 0 {
+		towers[right].Remove(towers[right].Back())
 	}
 }
 
 func printTower(left, center, right *list.List) {
 	fmt.Println("--------------------------------")
+	fmt.Print("l: ")
 	printOneTower(left)
+	fmt.Print("c: ")
 	printOneTower(center)
+	fmt.Print("r: ")
 	printOneTower(right)
 	fmt.Println("--------------------------------")
 }
@@ -92,4 +82,30 @@ func pop(l *list.List) (int, error) {
 
 func push(l *list.List, n int) {
 	l.PushBack(n)
+}
+
+func threeTower() {
+	//// 2
+	//// 3 _ 1
+	popPush(towers[left], towers[right])
+	////
+	//// 3 2 1
+	popPush(towers[left], towers[center])
+	////   1
+	//// 3 2 _
+	popPush(towers[right], towers[center])
+	////   1
+	//// _ 2 3
+	popPush(towers[left], towers[right])
+	////
+	//// 1 2 3
+	popPush(towers[center], towers[left])
+	////     2
+	//// 1 _ 3
+	popPush(towers[center], towers[right])
+	////     1
+	////     2
+	//// _ _ 3
+	popPush(towers[left], towers[right])
+	printTower(towers[left], towers[center], towers[right])
 }
