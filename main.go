b7240574f8d5bba6278af4db466277b7f0ac24ed
tagasouichiro <stitow14@gmail.com>
2017-02-12 20:39:49 +0900

    add build hanoi tower(3 or 4)

--- main.go
+++ main.go
@@ -17,38 +17,112 @@ var towers = []*list.List{
 	list.New(), // right tower
 }
 
+var count int
+
 func main() {
 	fmt.Println("left -> right pool")
-	initTower(towers, 3)
-	printTower(towers[left], towers[center], towers[right])
 
-	threeTower()
+	// 3
 	initTower(towers, 3)
-	printTower(towers[left], towers[center], towers[right])
+	fmt.Println("Init: 3")
+	printTower(towers)
+	//towerCalc(towers, towers[left].Len())
+
+	// 4
+	initTower(towers, 4)
+	fmt.Println("Init: 4")
+	printTower(towers)
+	towerCalc(towers, towers[left].Len())
+
+	// 5
+	initTower(towers, 5)
+	fmt.Println("Init: 5 ------------------------------------------")
+	printTower(towers)
+	towerCalc(towers, towers[left].Len())
+}
+
+func towerCalc(towers []*list.List, n int) {
+	if n == towers[right].Len() || count > 32 {
+		return
+	}
+	count++
+	lenl := towers[left].Len()
+	lenc := towers[center].Len()
+	lenr := towers[right].Len()
+	var len int
+	var start *list.List
+	var middle *list.List
+	var goal = towers[right]
+	if lenl > lenc && lenl > lenr {
+		fmt.Println("s:LEFT")
+		start = towers[left]
+		len = lenl
+		goal = towers[right]
+		middle = towers[center]
+	} else if lenc > lenr {
+		fmt.Println("s:CENTER")
+		start = towers[center]
+		len = lenc
+		goal = towers[right]
+		middle = towers[left]
+	} else {
+		fmt.Println("s:RIGHT")
+		start = towers[right]
+		len = lenr
+		goal = towers[center]
+		middle = towers[left]
+	}
+
+	if len%2 == 0 {
+		move(start, middle)
+		move(start, goal)
+		move(middle, goal)
+		move(start, middle)
+		move(goal, start)
+		move(goal, middle)
+		move(start, middle)
+		move(start, goal)
+	} else {
+		move(start, goal)
+		move(start, middle)
+		move(goal, middle)
+		move(start, goal)
+		move(middle, start)
+		move(middle, goal)
+		move(start, goal)
+	}
+
+	if middle.Len() == 0 && towers[right].Len() == 0 {
+		move(middle, towers[right])
+	}
+	towerCalc(towers, n)
 }
 
 func initTower(towers []*list.List, n int) {
-	for n > 0 {
-		towers[left].PushBack(n)
-		n--
+	for towers[left].Len() != 0 {
+		towers[left].Remove(towers[left].Back())
 	}
-
 	for towers[center].Len() != 0 {
 		towers[center].Remove(towers[center].Back())
 	}
 	for towers[right].Len() != 0 {
 		towers[right].Remove(towers[right].Back())
 	}
+
+	for n > 0 {
+		towers[left].PushBack(n)
+		n--
+	}
 }
 
-func printTower(left, center, right *list.List) {
+func printTower(tower []*list.List) {
 	fmt.Println("--------------------------------")
 	fmt.Print("l: ")
-	printOneTower(left)
+	printOneTower(tower[left])
 	fmt.Print("c: ")
-	printOneTower(center)
+	printOneTower(tower[center])
 	fmt.Print("r: ")
-	printOneTower(right)
+	printOneTower(tower[right])
 	fmt.Println("--------------------------------")
 }
 
@@ -61,11 +135,12 @@ func printOneTower(l *list.List) {
 	fmt.Println(output)
 }
 
-func popPush(po, pu *list.List) {
+func move(po, pu *list.List) {
 	n, err := pop(po)
 	if err == nil {
 		push(pu, n)
 	}
+	printTower(towers)
 }
 
 func pop(l *list.List) (int, error) {
@@ -87,25 +162,25 @@ func push(l *list.List, n int) {
 func threeTower() {
 	//// 2
 	//// 3 _ 1
-	popPush(towers[left], towers[right])
+	move(towers[left], towers[right])
 	////
 	//// 3 2 1
-	popPush(towers[left], towers[center])
+	move(towers[left], towers[center])
 	////   1
 	//// 3 2 _
-	popPush(towers[right], towers[center])
+	move(towers[right], towers[center])
 	////   1
 	//// _ 2 3
-	popPush(towers[left], towers[right])
+	move(towers[left], towers[right])
 	////
 	//// 1 2 3
-	popPush(towers[center], towers[left])
+	move(towers[center], towers[left])
 	////     2
 	//// 1 _ 3
-	popPush(towers[center], towers[right])
+	move(towers[center], towers[right])
 	////     1
 	////     2
 	//// _ _ 3
-	popPush(towers[left], towers[right])
-	printTower(towers[left], towers[center], towers[right])
+	move(towers[left], towers[right])
+	printTower(towers)
 }
